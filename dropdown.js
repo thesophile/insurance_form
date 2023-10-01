1688807240d8f1a10c29e22ab8acb4d120c92c70
The Sophile <abhinavpb249@gmail.com>
2023-10-01 19:53:17 +0530

    Almost completed the dropdown menu

--- dropdown.js
+++ dropdown.js
@@ -1,22 +1,32 @@
 const dropContainers = document.querySelectorAll('.dropContainer');
 
 
-
 dropContainers.forEach(dropContainer => {
-    console.log(dropContainer);
+
     const list = dropContainer.querySelector('.list');
-    console.log(list);
+    const selectedMonth = dropContainer.getElementsByClassName("selectedMonth")[0];
 
 
     dropContainer.addEventListener('click', ()=>{
         list.style.opacity = '1';
+        list.style.visibility="visible";
     })
 
     // Close the dropdown when a list item is clicked
     list.addEventListener('click', (event) => {
         if (event.target.tagName === 'LI') {
-        //   toggle.textContent = event.target.textContent;
-        list.style.opacity = '0';
+            event.stopPropagation();
+            selectedMonth.textContent = event.target.textContent; //set Text
+
+            list.style.opacity = '0'; //Hide list
+            list.style.visibility="hidden";
+
+            //move the label up 
+            const label = dropContainer.querySelector('label');
+            if (label) {
+              label.style.top = '0px'; 
+              label.style["font-size"] = '10px'; 
+            }
         }
     });
 
@@ -24,6 +34,7 @@ dropContainers.forEach(dropContainer => {
     document.addEventListener('click', (event) => {
         if (!dropContainer.contains(event.target) && !list.contains(event.target)) {
         list.style.opacity = '0';
+        list.style.visibility="hidden";
         }
     });
 });
