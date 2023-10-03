f990ce6e8499d3a5d9e8cb7b6786c91bb1b6870c
The Sophile <abhinavpb249@gmail.com>
2023-10-03 21:21:28 +0530

    Add files via upload

--- optionGenerator.js
+++ optionGenerator.js
@@ -5,26 +5,26 @@
         const yearSelect = document.getElementById('year');
 
         // Populate the day options (1 to 31)
-        function generateDayOptions() {
-            for (let day = 1; day <= 31; day++) {
-                const listElement = document.createElement('LI');
-                listElement.value = day;
-                listElement.textContent = day;
-                daySelect.appendChild(listElement);
-            }
-        }
+        // function generateDayOptions() {
+        //     for (let day = 1; day <= 31; day++) {
+        //         const option = document.createElement('option');
+        //         option.value = day;
+        //         option.textContent = day;
+        //         daySelect.appendChild(option);
+        //     }
+        // }
 
         // Function to generate year options 
-        function generateYearOptions() {
-            const currentYear = new Date().getFullYear();
-            for (let year = currentYear; year >= 1900; year--) {
-                const option = document.createElement('LI');
-                option.value = year;
-                option.textContent = year;
-                yearSelect.appendChild(option);
-            }
-        }
+        // function generateYearOptions() {
+        //     const currentYear = new Date().getFullYear();
+        //     for (let year = currentYear; year >= 1900; year--) {
+        //         const option = document.createElement('option');
+        //         option.value = year;
+        //         option.textContent = year;
+        //         yearSelect.appendChild(option);
+        //     }
+        // }
 
         // Initial population of year options
-        generateYearOptions();
-        generateDayOptions();
+        // generateYearOptions();
+        // generateDayOptions();
