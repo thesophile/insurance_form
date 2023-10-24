dca1c9db986a40978a7d3733012d6cb0501bb87d
The Sophile <abhinavpb249@gmail.com>
2023-10-24 21:21:31 +0530

    bug fix

--- script.js
+++ script.js
@@ -251,7 +251,7 @@ phoneNumberInput.addEventListener("input", function () {
 }
 
 if (input.length == 6) {
-  phoneNumberInput.value+= "-";
+  phoneNumberInput.value+= `+1 (${input.slice(0, 3)}) ${input.slice(4,7)}-`;
 }
 
 
